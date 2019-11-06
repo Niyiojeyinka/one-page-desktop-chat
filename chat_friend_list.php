6b4bd0569aca306caa20fec9d27bbc9a552138cd
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-11-06 13:32:08 +0100

    redesign search box

--- chat_friend_list.php
+++ chat_friend_list.php
@@ -2,7 +2,7 @@
 <div class="w3-row w3-center">
 	<div class="" style="display:inline; ">
 		<i class="fa fa-search w3-text-gray" style="display:inline;margin-right: -40px"></i>
-		<input type="search" name="search" placeholder="    Search..." class="w3-round-large w3-border w3-padding-top w3-padding-bottom w3-margin w3-transparent" size="30" style="outline:none;"/>
+		<input type="search" name="search" placeholder="        Search..." class="w3-round-large w3-border w3-padding-top w3-padding-bottom w3-margin w3-transparent" size="30" style="outline:none;padding-left: 36px"/>
 	</div>
 	<button class="w3-padding-small w3-border w3-round-large"><i class="fa fa-search w3-text-pink"></i></button>
 
