28ed6ea7f8cb9f440fb3997de97920f0004a1066
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-11-08 10:02:24 +0100

    added api.php file

---
+++ backend/Api.php
@@ -0,0 +1,16 @@
+<?php
+
+
+/**
+ * @package : Api.php
+ * @author : @niyiojeyinka
+ * @descriptions :" class to handle ajax/fetch function requests"
+ */
+class Api
+{
+	
+	public function __construct()
+	{
+		# code...
+	}
+}
