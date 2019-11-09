139ca91cb8668d0fcba54bae71e0673f1f278473
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-11-09 23:01:11 +0100

    added get profile endpoint

--- backend/Api.php
+++ backend/Api.php
@@ -6,14 +6,24 @@
  * @author : @niyiojeyinka
  * @descriptions :" class to handle ajax/fetch function requests"
  */
+
 require 'ChatProfile.php';
-class Api
-{
-	
+class Api {
+	  public $profile;
 	public function __construct()
 	{
-	 
+      $this->profile = new ChatProfile();
 	}
 
+
+    /*
+     *@parameter : id of user
+     * returns json consisting of the user's friends profiles
+     */
+    public function get_friends_profiles($id='')
+    {
+     
+     echo json_encode($this->profile->getFriendsProfiles($id),true);
+    }
 
 }
