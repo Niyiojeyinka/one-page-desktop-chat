8302b57812a56c489936580507083cb84e2427fb
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-11-09 22:30:53 +0100

    edited the route logic

--- apiroute.php
+++ apiroute.php
@@ -6,19 +6,19 @@ $indexOfIndexPHP = array_search("apiroute.php", $url_array);
 //Get position of apiroute.php in the link in case the tester tests using deep folder
 
 //Routing
-if (array_key_exists($indexOfIndexPHP + 1, $url_array) && $url_array[$indexOfIndexPHP + 1] != "") {
+if (array_key_exists($indexOfIndexPHP , $url_array) && $url_array[$indexOfIndexPHP] != "" && isset($url_array[$indexOfIndexPHP + 1])) {
 	//If url as first parameter and the parameter is not /
 	
-	require_once "dashboard.php";
-			$dashboard = new Dashboard;
-			$method = $url_array[$indexOfIndexPHP + 2];
-			$parameter = isset($url_array[$indexOfIndexPHP + 3]) ? $url_array[$indexOfIndexPHP + 3]: NULL;
-			$dashboard->$method($parameter);
-			/*modified so that /apiroute.php/dashboard/totalExpenses/4
-				will call the totalExpenses function and insert 4 as parameter 
+	require_once "backend/api.php";
+			$api = new Api;
+			$method = $url_array[$indexOfIndexPHP + 1];
+			$parameter = isset($url_array[$indexOfIndexPHP + 2]) ? $url_array[$indexOfIndexPHP + 2]: NULL;
+			$api->$method($parameter);
+			/*modified so that /apiroute.php/getProfile/4
+				will call the getProfile method and insert 4 as parameter 
 			*/
 }else {
   //home
-	echo "This Home, Direct Access Not Allowed";
+	echo "This Home, Direct Access Not Allowed This is for both ajax and Fetch API only";
 }
 ?>
