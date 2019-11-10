93c0e11961f71526f76f42076b53373f99c0ee7c
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-11-10 11:06:53 +0100

    added status indicator

--- js/profile.js
+++ js/profile.js
@@ -6,7 +6,7 @@ function htmlFriendListTemplate(profile) {
 return `<div class="w3-bar w3-padding w3-dispay-container w3-border-bottom">
 	<img class="w3-image w3-circle w3-bar-item" src="./images/${profile.profile_picture}" style="width:50px;height:50px;padding:0;" />
 	<div class="w3-bar-item" style="padding:0px;margin-left:12px;margin-top:2px;width: 80%;">
-		<span class=" w3-text-black" style="font-size:13px;">${profile.firstname} ${profile.lastname}<i class="fa fa-circle w3-text-green w3-tiny" style="margin-left: 2px;"></i></span>
+		<span class=" w3-text-black" style="font-size:13px;">${profile.firstname} ${profile.lastname}<i class="fa fa-circle ${profile.status =="online"? "w3-text-green":"w3-text-gray"} w3-tiny" style="margin-left: 2px;"></i></span>
 		<div class="w3-right w3-center">
 		<span class="w3-small w3-text-gray">${profile.lastMessageTime}</span><br>
         <span class="w3-circle w3-green w3-padding-small w3-tiny w3-right" style="">${profile.unreadMessages}</span>
