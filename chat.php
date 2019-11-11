4eaf9ebc32c62c1a6749798c00fc25114e0c8637
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-11-11 03:31:59 +0100

    added default message to tthe chat column

--- chat.php
+++ chat.php
@@ -5,50 +5,9 @@
 
 <!--MAIN PAGE CONTENT STARTS HERE-->
 
-  <div class="mine messages">
-    <div class="message last">
-      Dude
-     </div>
-  </div>
-  <div class="yours messages">
-    <div class="message">
-      Hey!
-    </div>
-    <div class="message">
-      You there?
-    </div>
-    <div class="message last">
-      Hello, how's it going?
-    </div>
-  </div>
-  <div class="mine messages">
-    <div class="message">
-      Great thanks!
-    </div> 
-    <div class="message last">
-      How about you?
-     </div>
-  </div>
 
-
- 
-	<!--MAIN PAGE CONTENT ENDS HERE-->
-
-
-	<div class='w3-panel w3-right w3-bar' style='margin:0;'>
-	   <div class='w3-bar-item w3-animate-zoom w3-display-container'style='width:150px;height:250px;padding:0;'>
-		<img src='chatplaceholder1.jpg' class='w3-round-xxlarge w3-card w3-image w3-ripple'style='width:100%;height:100%;'>
-		<div class='w3-display-middle w3-text-white w3-center w3-display-container'style='width:50px;'>
-		<i class='fa fa-spinner w3-xxxlarge w3-spin w3-text-pink'></i>
-		<span class='w3-display-middle w3-small'>10%</span>
-	    </div>
-	    </div>
-
-	   
-	
-
-	</div>	
-
+<br><br>
+<span>Click user profile to view Conversation</span>
 
 
 
